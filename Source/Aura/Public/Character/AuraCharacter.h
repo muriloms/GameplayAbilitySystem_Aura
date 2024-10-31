2eb388f80ef623c4f7495f11dbd2afb334d322ee
muriloms <ms7murilo@gmail.com>
2024-10-31 17:57:39 -0300

    Gameplay Ability System

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -13,5 +13,15 @@ UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
+
+public:
+
+	AAuraCharacter();
+
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
 	
+private:
+
+	void InitAbilityActorInfo();
 };
